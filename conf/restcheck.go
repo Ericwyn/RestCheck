1bdfb5eff268993dd4c6f7b6ad4fb578de89351a
Ericwyn <16485562+Ericwyn@users.noreply.github.com>
2023-11-29 20:18:12 +0800

    [conf] add default config msg

--- conf/restcheck.go
+++ conf/restcheck.go
@@ -14,6 +14,10 @@ type EnvSetting struct {
 	BaseUrl string
 }
 
+type ApiConfig struct {
+	IgnoreCheck []string // 需要忽略校验的字段, 用比如 result.data.createTime 来记录
+}
+
 // 检查 commend config 是否正确
 // 如果没问题就返回 "", 否则返回异常信息
 func (config *ProjectConfig) check() string {
@@ -22,4 +26,14 @@ func (config *ProjectConfig) check() string {
 	// 检查 DefaultEnv 是否在 envs 里面
 
 	return ""
+}
+
+func BuildDefaultProjectConfig() ProjectConfig {
+	return ProjectConfig{
+		ProjectName: "RestCheck",
+		Header: map[string]string{
+			"Content-Type": "",
+		},
+		BaseUrl: "http://127.0.0.1:3579",
+	}
 }
